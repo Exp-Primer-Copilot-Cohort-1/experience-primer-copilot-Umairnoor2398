086931430c2de426d4cf0df092f8dd584380af73
Umair Noor <88612698+Umairnoor2398@users.noreply.github.com>
2023-10-20 19:49:13 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,18 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    var projects = document.getElementById("projects");
+    var contact = document.getElementById("contact");
+    var memberDiv = document.getElementById("memberDiv");
+    var skillsDiv = document.getElementById("skillsDiv");
+    var projectsDiv = document.getElementById("projectsDiv");
+    var contactDiv = document.getElementById("contactDiv");
+    member.style.backgroundColor = "#e6e6e6";
+    skills.style.backgroundColor = "#f2f2f2";
+    projects.style.backgroundColor = "#f2f2f2";
+    contact.style.backgroundColor = "#f2f2f2";
+    memberDiv.style.display = "block";
+    skillsDiv.style.display = "none";
+    projectsDiv.style.display = "none";
+    contactDiv.style.display = "none";
+}
